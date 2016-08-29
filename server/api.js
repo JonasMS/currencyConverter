d0e6142cb08e9305cb8de1d9f7f63a39c4ca033a
JonasMS <jonas.sota@gmail.com>
2016-08-29 12:32:26 -0700

    First Commit

--- server/api.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = new Router();
-
-router.get('/', (req, res) => {
-  console.log('req received');
-  res.send();
-});
-
-module.exports = router;
--- server/config/middleware.js
+++
@@ -1,5 +0,0 @@
-module.exports = (app, express) => {
-  app.use(express.static(__dirname + '../'));
-  app.use(express.static(__dirname + '../build'));
-  console.log('server statics');
-}
